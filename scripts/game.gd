dc51544ba0cd4096aaceb5da30d54fd584317f14
Juan Medina <jamedina@gmail.com>
2024-12-22 19:54:23 +0000

    making bg

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("toggle_fullscreen"):
@@ -17,3 +17,12 @@ func _process(_delta: float) -> void:
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_size(get_viewport_rect().size)
+	move_background(delta)
+
+
+@onready var bg = $BG
+
+
+func move_background(delta):
+	bg.scroll_offset.x -= 300 * delta
+	pass
