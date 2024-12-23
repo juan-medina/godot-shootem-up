1aa244e4c880ca841e6d23587469f9415a228503
Juan Medina <jamedina@gmail.com>
2024-12-23 15:57:44 +0000

    change ship sprite

--- scripts/bg.gd
+++ scripts/bg.gd
@@ -3,6 +3,5 @@ class_name Background extends ParallaxBackground
 @export var scroll_speed: int = 300
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	scroll_offset.x -= scroll_speed * delta
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,11 +3,11 @@ class_name Player extends CharacterBody2D
 @export var speed: int = 650
 
 var previous_direction: Vector2 = Vector2.ZERO
+var previous_exhaust: String = "normal"
+@onready var exhaust_anim = $Exhaust
 
-
-func _process(delta: float) -> void:
-	calculate_anim(delta)
-
+func _ready() -> void:
+	exhaust_anim.play(previous_exhaust)
 
 func _physics_process(_delta: float) -> void:
 	var x_axis: float = Input.get_axis("left", "right")
@@ -24,55 +24,21 @@ func _physics_process(_delta: float) -> void:
 	clamp_position()
 
 
-const idle_frame: int = 0
-const up_frame: int = 6
-const down_frame: int = 6
-
-var current_frame: float = idle_frame
-var desired_frame: float = idle_frame
-
-@onready var ship_anim = $Ship
-
-
-func calculate_anim(delta: float) -> void:
-	var frame_dir: float = desired_frame - current_frame
-
-	if frame_dir != 0:
-		frame_dir = frame_dir / abs(frame_dir)
-
-	current_frame += frame_dir * delta * 10
-
-	ship_anim.frame = roundi(current_frame)
-
-
+@onready var ship_sprite = $Ship
 @onready var clamp_max: Vector2 = get_viewport_rect().size
 
 
 func clamp_position() -> void:
-	var current_animation: String = ship_anim.animation
-	var sprite_texture = ship_anim.sprite_frames.get_frame_texture(current_animation, ship_anim.frame)
-	var half_size = sprite_texture.get_size() / 2 * ship_anim.scale
+	var half_size = ship_sprite.texture.get_size() / 2
 	position = position.clamp(half_size, clamp_max - half_size)
 
 
-var previous_exhaust: String = "low"
-@onready var exhaust_anim = $exhaust
-
-
 func direction_changed(direction: Vector2) -> void:
-	if direction.y > 0:
-		desired_frame = up_frame
-	elif direction.y < 0:
-		desired_frame = down_frame
-	else:
-		desired_frame = idle_frame
-
-	var exhaust: String = "low"
+	var exhaust: String = "normal"
 
 	if direction.x > 0 || direction.y != 0:
-		exhaust = "high"
+		exhaust = "turbo"
 
 	if previous_exhaust != exhaust:
 		exhaust_anim.play(exhaust)
 		previous_exhaust = exhaust
-
