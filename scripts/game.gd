153f005af8975633ce7b3d8511ea462cffbf9a28
Juan Medina <jamedina@gmail.com>
2024-12-23 07:14:18 +0000

    add scroll speed as parameter

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,17 +1,10 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass  # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("toggle_fullscreen"):
-		var currrent_mode = DisplayServer.window_get_mode()
+		var currrent_mode: int = DisplayServer.window_get_mode()
 		if currrent_mode == DisplayServer.WINDOW_MODE_WINDOWED:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		else:
@@ -21,8 +14,8 @@ func _process(delta: float) -> void:
 
 
 @onready var bg = $BG
-
+@export var scroll_speed : int = 300
 
 func move_background(delta):
-	bg.scroll_offset.x -= 300 * delta
-	pass
+	bg.scroll_offset.x -= scroll_speed * delta
+
