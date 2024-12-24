b3e911cd4193f83db180d5cba81d4cee6e0774d4
Juan Medina <jamedina@gmail.com>
2024-12-24 23:21:40 +0000

    adding a enemy that we can hit

---
+++ scenes/enemies/basic_enemy.gd
@@ -0,0 +1,32 @@
+class_name BasicEnemy extends Area2D
+
+func _on_area_entered(object: Area2D) -> void:
+	if object is PlayerShot:
+		add_hit_effect()
+		object.queue_free()
+
+
+@export var hit_duration: float = 1.0
+
+
+@onready var hit_material: ShaderMaterial = preload("res://resources/materials/hit.tres")
+
+
+var remove_material_timer: Timer = null
+
+
+func add_hit_effect() -> void:
+	if remove_material_timer == null:
+		remove_material_timer = Timer.new()
+		remove_material_timer.one_shot = true
+		remove_material_timer.timeout.connect(_on_remove_material_timer_timeout.bind())
+		add_child(remove_material_timer)
+
+	if remove_material_timer.is_stopped():
+		self.material = hit_material
+	else:
+		remove_material_timer.stop()
+	remove_material_timer.start(hit_duration)
+
+func _on_remove_material_timer_timeout() -> void:
+	self.material = null
