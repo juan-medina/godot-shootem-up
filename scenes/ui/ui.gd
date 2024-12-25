50c078d70d981e397eddc721d04a5ffa4fe89430
Juan Medina <jamedina@gmail.com>
2024-12-25 23:58:48 +0000

    adding a simple version display in the UI

--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -1,7 +1,13 @@
 class_name UI extends Control
 
-# create a property named points with a setter function
 @export var points: int = 0:
 	set(value):
 		points = value
 		$Points.text = str(points)
+
+func _ready() -> void:
+	var version_split = ProjectSettings.get_setting("application/config/version").split(".")
+	$Version/Major.text = version_split[0]
+	$Version/Minor.text = version_split[1]
+	$Version/Patch.text = version_split[2]
+	$Version/Build.text = version_split[3]
