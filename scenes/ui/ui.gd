8043e9de44aad0ea0259b2af69c64ac229fc628c
Juan Medina <jamedina@gmail.com>
2024-12-26 05:24:11 +0000

    increment version on running the project

--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -6,8 +6,16 @@ class_name UI extends Control
 		$Points.text = str(points)
 
 func _ready() -> void:
-	var version_split = ProjectSettings.get_setting("application/config/version").split(".")
-	$Version/Major.text = version_split[0]
-	$Version/Minor.text = version_split[1]
-	$Version/Patch.text = version_split[2]
-	$Version/Build.text = version_split[3]
+	var version = ProjectSettings.get_setting("application/config/version").split(".")
+
+	if OS.is_debug_build():
+		version[3] = str(int(version[3]) + 1)
+		var new_version = "%s.%s.%s.%s" % [version[0], version[1], version[2], version[3]]
+		ProjectSettings.set_setting("application/config/version", new_version)
+		ProjectSettings.save()
+
+	$Version/Major.text = version[0]
+	$Version/Minor.text = version[1]
+	$Version/Patch.text = version[2]
+	$Version/Build.text = version[3]
+
