8e9837967424c24302ec6516fb40fe2d62f1327e
Juan Medina <jamedina@gmail.com>
2024-12-26 15:00:59 +0000

    increase build number via a plugin

--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -11,12 +11,6 @@ class_name UI extends Control
 func _ready() -> void:
 	var version = ProjectSettings.get_setting("application/config/version").split(".")
 
-	if OS.is_debug_build():
-		version[3] = str(int(version[3]) + 1)
-		var new_version = "%s.%s.%s.%s" % [version[0], version[1], version[2], version[3]]
-		ProjectSettings.set_setting("application/config/version", new_version)
-		ProjectSettings.save()
-
 	$Version/Major.text = version[0]
 	$Version/Minor.text = version[1]
 	$Version/Patch.text = version[2]
