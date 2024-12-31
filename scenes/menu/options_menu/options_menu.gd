bce90d5e5def3b55884945fabf73fa62f3482f0b
Juan Medina <jamedina@gmail.com>
2024-12-31 15:13:20 +0000

    working on saving options #4

--- scenes/menu/options_menu/options_menu.gd
+++ scenes/menu/options_menu/options_menu.gd
@@ -24,6 +24,20 @@ extends SubMenu
 ##
 ## The menu that allows the player to change the game settings
 
+enum OptionsDisplayMode { WINDOWED, FULLSCREEN }
+
+var display_mode: OptionsDisplayMode = OptionsDisplayMode.WINDOWED:
+	set(value):
+		_windowed_check.set_pressed(value == OptionsDisplayMode.WINDOWED)
+		_full_screen_check.set_pressed(value == OptionsDisplayMode.FULLSCREEN)
+	get():
+		if _windowed_check.is_pressed():
+			return OptionsDisplayMode.WINDOWED
+		return OptionsDisplayMode.FULLSCREEN
+
 @onready var ok_button: Button = $Buttons/Ok  ## Ok button
 @onready var back_button: Button = $Buttons/Back  ## Back button
 @onready var apply_button: Button = $Buttons/Apply  ## Apply button
+
+@onready var _windowed_check: CheckButton = $Panel/VFlowContainer/DisplayModeRow/WindowedCheck
+@onready var _full_screen_check: CheckButton = $Panel/VFlowContainer/DisplayModeRow/FullScreenCheck
