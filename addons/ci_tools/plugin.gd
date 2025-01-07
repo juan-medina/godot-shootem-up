652b1ecdc275c31b095c4ffb908eb9b9d3bbdfbc
Juan Medina <jamedina@gmail.com>
2025-01-07 09:38:11 +0000

    generate about bbcode from credits.json #29

--- addons/ci_tools/plugin.gd
+++ addons/ci_tools/plugin.gd
@@ -26,21 +26,30 @@ extends EditorPlugin
 ## This plugin adds a tool menu item to the editor, and a shortcut, that will
 ## increase the build number and launch the main scene.
 
-const _TOOL_MENU: String = "Build Version: Increase And Launch Main Scene (CTRL+F5)"  ## Tool menu text
+const _VERSION_INCREASE_MENU: String = "Build Version: Increase And Launch Main Scene (CTRL+F5)"  ## Version increase menu text
+const _UPDATE_CREDITS_MENU: String = "Update Credits"  ## Update credits menu text
 const _VERSION_KEY: String = "application/config/version"  ## Where the build version is stored
+
+const _CREDITS_JSON_FILE: String = "res://resources/credits/credits.json"  ## The JSON file with the credits data
+
+const _BBCODE_TEMPLATE_FILE: String = "res://resources/credits/about_template.bbcode"  ## The template file for the about BBCode
+const _ABOUT_BBCODE_FILE: String = "res://resources/credits/about.bbcode"  ## The output file for the about BBCode
+
 var _shortcut: Shortcut = preload("res://addons/ci_tools/shortcut.tres")  ## The shortcut to use
 
 
 ## Plugin enabled
 func _enter_tree() -> void:
-	# add our tool menu item to our function
-	add_tool_menu_item(_TOOL_MENU, _increase_build_and_launch)
+	# add our tool menus item to our functions
+	add_tool_menu_item(_VERSION_INCREASE_MENU, _increase_build_and_launch)
+	add_tool_menu_item(_UPDATE_CREDITS_MENU, _update_credits)
 
 
 ## Plugin disabled
 func _exit_tree() -> void:
 	# remove our tool menu item
-	remove_tool_menu_item(_TOOL_MENU)
+	remove_tool_menu_item(_VERSION_INCREASE_MENU)
+	remove_tool_menu_item(_UPDATE_CREDITS_MENU)
 
 
 ## When we get any shortcut pressed
@@ -69,3 +78,61 @@ func _increase_build_and_launch() -> void:
 
 	# launch the main scene
 	EditorInterface.play_main_scene()
+
+
+## Update the credits menu item
+func _update_credits() -> void:
+	# Read JSON file
+	var json_file: FileAccess = FileAccess.open(_CREDITS_JSON_FILE, FileAccess.READ)
+	if not json_file:
+		push_error("JSON file not found: %s" % _CREDITS_JSON_FILE)
+		return
+	var json_data: String = json_file.get_as_text()
+	json_file.close()
+	var credits_data: Dictionary = JSON.parse_string(json_data)
+
+	# Generate the about BBCode
+	_generate_about_bbcode(credits_data)
+
+
+## Generate the about BBCode
+func _generate_about_bbcode(credits_data: Dictionary) -> void:
+	# Generate BBCode for credits
+	var bbcode: String = ""
+	for credit in credits_data["credits"]:
+		bbcode += "- %s: [url=%s][color=#59B0F0]%s[/color][/url]" % [credit["role"], credit["url"], credit["name"]]
+		if "author" in credit:
+			bbcode += " by [url=%s][color=#59B0F0]%s[/color][/url]" % [credit["author"]["url"], credit["author"]["name"]]
+		bbcode += "."
+		if "details" in credit:
+			for detail in credit["details"]:
+				bbcode += "\n    - %s: %s." % [detail["type"], detail["name"]]
+		bbcode += "\n"
+
+	# Remove the last newline character
+	if bbcode.ends_with("\n"):
+		bbcode = bbcode.substr(0, bbcode.length() - 1)
+
+	## Write the about BBCode file
+	_write_file(_BBCODE_TEMPLATE_FILE, _ABOUT_BBCODE_FILE, {"CREDITS": bbcode})
+
+
+## Write a output file using a template file and the giving data
+func _write_file(template_path: String, output_path: String, replacements: Dictionary) -> void:
+	# Read template file
+	var template_file: FileAccess = FileAccess.open(template_path, FileAccess.READ)
+	if not template_file:
+		push_error("Template file not found: %s" % template_path)
+		return
+	var template_text: String = template_file.get_as_text()
+	template_file.close()
+
+	# Do template replacements
+	for data in replacements:
+		var key: String = "%%" + data + "%%"
+		template_text = template_text.replace(key, replacements[data])
+
+	# Write to output file
+	var output_file: FileAccess = FileAccess.open(output_path, FileAccess.WRITE)
+	output_file.store_string(template_text)
+	output_file.close()
